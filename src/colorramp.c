f3c99751d544f5d9482a55c1c53653f36201634e
Daniel Georgiev <daniel.georgiev95@gmail.com>
2016-05-16 00:05:45 +0300

    Removing comments at the beginning of the source

--- src/colorramp.c
+++ src/colorramp.c
@@ -1,23 +1,3 @@
-/* colorramp.c -- color temperature calculation source
-   This file is part of Redshift.
-
-   Redshift is free software: you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation, either version 3 of the License, or
-   (at your option) any later version.
-
-   Redshift is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
-
-   You should have received a copy of the GNU General Public License
-   along with Redshift.  If not, see <http://www.gnu.org/licenses/>.
-
-   Copyright (c) 2013-2014  Jon Lund Steffensen <jonlst@gmail.com>
-   Copyright (c) 2013  Ingo Thies <ithies@astro.uni-bonn.de>
-*/
-
 #include <stdint.h>
 #include <math.h>
 
