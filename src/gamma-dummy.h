3b202f63b8e99b27e7d8a9a8004752d4d5b74c13
Daniel Georgiev <daniel.georgiev95@gmail.com>
2016-05-19 23:27:05 +0300

    Improving Iris colors lite

--- src/gamma-dummy.h
+++ src/gamma-dummy.h
@@ -4,19 +4,21 @@
 ////////////////////////////////////////////////////////////
 /* gamma-dummy.h -- No-op gamma adjustment header */
 
+////////////////////////////////////////////////////////////
 #include "redshift.h"
 
-
+////////////////////////////////////////////////////////////
 int gamma_dummy_init(void *state);
 int gamma_dummy_start(void *state);
 void gamma_dummy_free(void *state);
 
+////////////////////////////////////////////////////////////
 void gamma_dummy_print_help(FILE *f);
 int gamma_dummy_set_option(void *state, const char *key, const char *value);
 
+////////////////////////////////////////////////////////////
 void gamma_dummy_restore(void *state);
-int gamma_dummy_set_temperature(void *state,
-				const color_setting_t *setting);
+int gamma_dummy_set_temperature(void *state, const color_setting_t *setting);
 
 ////////////////////////////////////////////////////////////
 #endif /* ! IRIS_GAMMA_DUMMY_H */
